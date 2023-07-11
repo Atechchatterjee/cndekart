98d3d01df4762e00e9c8acd252a37ffe4c030a58
Atechchatterjee <atechchatterjeee@gmail.com>
2023-07-11 09:21:08 +0530

    feat: made this a client component

--- app/admin/product/ProductList.tsx
+++ app/admin/product/ProductList.tsx
@@ -1,10 +1,10 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { prisma } from "utils/prisma-client";
 import { RiEditBoxFill } from "react-icons/ri";
 import DeleteButton from "./DeleteButton";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
-async function ProductImage({
-  productId,
+function ProductImage({
   productImages,
 }: {
   productId: string;
@@ -12,7 +12,6 @@ async function ProductImage({
 }) {
   const WIDTH = 200,
     HEIGHT = 150;
-  // return JSON.stringify({ productImages }, null, 2);
   if (productImages.length > 0)
     return (
       <img
@@ -25,16 +24,13 @@ async function ProductImage({
     );
 }
 
-export default async function ProductList() {
-  const productLists = await prisma.product.findMany({
-    include: {
-      images: {},
-      categoryRelation: {},
-      priceRelation: {},
-      unitRelation: {},
-    },
-  });
-
+export default function ProductList({
+  productLists,
+  isLoading,
+}: {
+  productLists: any[] | undefined;
+  isLoading: boolean;
+}) {
   return (
     <div className="flex flex-col gap-3 mt-10">
       <div className="flex flex-col gap-2">
@@ -45,37 +41,42 @@ export default async function ProductList() {
           List of all product that has been created.
         </p>
       </div>
-      {productLists.map((product, i) => (
-        <div
-          className="flex gap-10 border border-slate-200 rounded-lg p-5 h-[14rem] w-full mt-5 hover:border-slate-300 transition-all"
-          key={i}
-        >
-          {/* @ts-ignore-error Server Component */}
-          <ProductImage productId={product.id} productImages={product.images} />
-          <div className="flex flex-col gap-7">
-            <div className="flex flex-col gap-3">
-              <div className="flex flex-col gap-2">
-                <h2 className="font-bold text-[1.2rem]">{product.title}</h2>
-                <p className="font-slate-200">{product.description}</p>
+      {isLoading ? (
+        <LoadingSpinner className="self-center ml-auto mr-auto" />
+      ) : (
+        productLists?.map((product, i) => (
+          <div
+            className="flex gap-10 border border-slate-200 rounded-lg p-5 h-[14rem] w-full mt-5 hover:border-slate-300 transition-all"
+            key={i}
+          >
+            {/* @ts-ignore-error Server Component */}
+            <ProductImage productImages={product.images} />
+            <div className="flex flex-col gap-7">
+              <div className="flex flex-col gap-3">
+                <div className="flex flex-col gap-2">
+                  <h2 className="font-bold text-[1.2rem]">{product.title}</h2>
+                  <p className="font-slate-200">{product.description}</p>
+                </div>
+                <div className="flex gap-2">
+                  <span className="font-semibold">Price: </span>
+                  <span>{product?.priceRelation[0]?.range}: </span>
+                  <span>
+                    ₹{product.priceRelation[0].price}/
+                    {product.unitRelation?.unit}
+                  </span>
+                </div>
               </div>
-              <div className="flex gap-2">
-                <span className="font-semibold">Price: </span>
-                <span>{product.priceRelation[0]?.range}: </span>
-                <span>
-                  ₹{product.priceRelation[0].price}/{product.unitRelation?.unit}
-                </span>
+              <div className="flex gap-3">
+                <Button variant="primary" className="w-[8rem] gap-2" size="sm">
+                  <RiEditBoxFill />
+                  Edit
+                </Button>
+                <DeleteButton product={product} />
               </div>
-            </div>
-            <div className="flex gap-3">
-              <Button variant="primary" className="w-[8rem] gap-2" size="sm">
-                <RiEditBoxFill />
-                Edit
-              </Button>
-              <DeleteButton product={product} />
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }
